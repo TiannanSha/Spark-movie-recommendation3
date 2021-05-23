d1e4476f066042555315c990641f073c57c2d012
Tiannan Sha <tiannansha@gmail.com>
2021-05-23 22:46:40 +0200

    implement knn predictor using breeze linear algebra library

--- economics/src/main/scala/Economics.scala
+++ economics/src/main/scala/Economics.scala
@@ -17,6 +17,33 @@ object Economics {
 
     var conf = new Conf(args)
 
+    // ***** my code starts here *****
+
+    // *** Q5.1.1 ***
+    val ICCM7Rent = 20.4
+    val ICCMBuy = 35000.0
+    val minDays = ICCMBuy / ICCM7Rent
+    val minYears = minDays/365.0
+
+    // *** Q5.1.2 ***
+    val ICCM7numCore = 2*14
+    val ramICCM7 = 1500   // GB
+    val dailyCostContainer = ramICCM7*0.012 + ICCM7numCore*0.092
+    val ratioICCM7Container = ICCM7Rent/dailyCostContainer
+
+    val containerCheaperRatio = (ICCM7Rent - dailyCostContainer) / ICCM7Rent
+    val containerCheaper = containerCheaperRatio > 0.05
+
+    // *** Q5.1.3 ***
+    val numCore4pri = 4*4
+    val ram4rpi = 4*8  // GB
+    val dailyCostContainerQ513 = ram4rpi*0.012 + numCore4pri*0.092
+    //val dailyCost4rpiMax =
+    val ratioICCM7ContainerQ513 = ICCM7Rent/dailyCostContainer
+
+    val containerCheaperRatio = (ICCM7Rent - dailyCostContainer) / ICCM7Rent
+    val containerCheaper = containerCheaperRatio > 0.05
+
     // Save answers as JSON
     def printToFile(content: String,
                     location: String = "./answers.json") =
@@ -35,13 +62,13 @@ object Economics {
 
           val answers: Map[String, Any] = Map(
             "Q5.1.1" -> Map(
-              "MinDaysOfRentingICC.M7" -> 0.0, // Datatype of answer: Double
-              "MinYearsOfRentingICC.M7" -> 0.0 // Datatype of answer: Double
+              "MinDaysOfRentingICC.M7" -> minDays, // Datatype of answer: Double
+              "MinYearsOfRentingICC.M7" -> minYears // Datatype of answer: Double
             ),
             "Q5.1.2" -> Map(
-              "DailyCostICContainer_Eq_ICC.M7_RAM_Throughput" -> 0.0, // Datatype of answer: Double
-              "RatioICC.M7_over_Container" -> 0.0, // Datatype of answer: Double
-              "ContainerCheaperThanICC.M7" -> true // Datatype of answer: Boolean
+              "DailyCostICContainer_Eq_ICC.M7_RAM_Throughput" -> dailyCostContainer, // Datatype of answer: Double
+              "RatioICC.M7_over_Container" -> ratioICCM7Container, // Datatype of answer: Double
+              "ContainerCheaperThanICC.M7" -> containerCheaper // Datatype of answer: Boolean
             ),
             "Q5.1.3" -> Map(
               "DailyCostICContainer_Eq_4RPi4_Throughput" -> 0.0, // Datatype of answer: Double
--- optimizing/src/main/scala/Predictor.scala
+++ optimizing/src/main/scala/Predictor.scala
@@ -30,6 +30,10 @@ object Predictor {
     for (line <- trainFile.getLines) {
         val cols = line.split(conf.separator()).map(_.trim)
         trainBuilder.add(cols(0).toInt-1, cols(1).toInt-1, cols(2).toDouble)
+//      println(f"cols(0).toInt-1 = ${cols(0).toInt-1}")
+//      println(f"cols(1).toInt-1 = ${cols(1).toInt-1}")
+//      println(f"cols(2).toDouble = ${cols(2).toDouble}")
+
     }
     val train = trainBuilder.result()
     trainFile.close
@@ -37,10 +41,122 @@ object Predictor {
     println("Read data in " + (read_duration/pow(10.0,9)) + "s")
 
     println("Compute kNN on train data...")
-    
+
+    // my code starts here
+    val num_u = train.rows.toInt
+    val num_i = train.cols.toInt
+    println(f"num_u = ${num_u}")
+
+    // helper functions
+    def scale(x:Double, ru:Double): Double = {
+      if (x>ru) {
+        5-ru
+      } else if (x<ru) {
+        ru-1
+      } else {
+        1
+      }
+    }
+
+    val t = System.nanoTime()
+
+    // *** calculate ru_， average rating for each user ***
+    var ru_arr = new Array[Double](num_u)   // user rating array
+    for (i <- ru_arr.indices) {ru_arr(i) = 0.0}
+    var counts = new Array[Int](num_u) // count number of ratings per user
+    for (i <- counts.indices) {counts(i) = 0}
+    for ((k,rui) <- train.activeIterator) {
+      val u = k._1
+      val i = k._2
+      ru_arr(u) += rui
+      counts(u) += 1
+    }
+    for (i <- ru_arr.indices) {ru_arr(i) = ru_arr(i)/counts(i)}
+    //val ru_ = new DenseVector(ru_arr)
+    //println(s"ru_ = ${ru_}")
+
+    //*** rhat_ui, each rating's normalized deviation ***
+    val rhatui_Builder = new CSCMatrix.Builder[Double](rows=num_u, cols=num_i)
+    for ((k,rui) <- train.activeIterator) {
+      val u = k._1
+      val i = k._2
+      val rhat_ui = (rui - ru_arr(u)) / scale(rui, ru_arr(u))
+      rhatui_Builder.add(u, i, rhat_ui)
+    }
+    val rhatui_m = rhatui_Builder.result()
+
+
+    // **** rcapui, the preprocessed rating ***
+    // calculate denom
+    var sumOfSqrs = new Array[Double](num_u)
+    for (i <- sumOfSqrs.indices) {sumOfSqrs(i) = 0.0}
+    for ((k,rhatui) <- rhatui_m.activeIterator) {
+      val u = k._1
+      val i = k._2
+      sumOfSqrs(u) += rhatui * rhatui
+    }
+    val rcapui_Builder = new CSCMatrix.Builder[Double](rows=num_u, cols=num_i)
+    for ((k,rhatui) <- rhatui_m.activeIterator) {
+      val u = k._1
+      val i = k._2
+      val rcap_ui = rhatui / math.sqrt(sumOfSqrs(u))
+      rcapui_Builder.add(u, i, rcap_ui)
+    }
+    val rcapui_m = rcapui_Builder.result()
+    //println(rcapui_m)
+
+    // *** calculate similarities ***
+    val k = 200
+    val sims_Builder = new CSCMatrix.Builder[Double](rows=num_u, cols=num_u)
+    for (u <- 0 until rcapui_m.rows) {
+      val vec_for_u = rcapui_m(u, 0 until rcapui_m.cols).t.toDenseVector
+      val simVec_for_u = rcapui_m * vec_for_u
+      for (v <- argtopk(simVec_for_u, k+1)) {
+        //println(u,v, simVec_for_u(v))
+        if (u!=v) {
+          sims_Builder.add(u, v, simVec_for_u(v))
+        }
+      }
+    }
+    val sims_m = sims_Builder.result()
+
+    val duration_knnSims = (System.nanoTime() - t) / 1e9
+    println(f"duration_knnSims = $duration_knnSims")
+
+    // *** compute rbarhat, the user-specific weighted-sum deviation, eq.2 in project2 ***
+//    val rbarhat_Builder = new CSCMatrix.Builder[Double](rows=num_u, cols=num_i)
+//    val eq2num = sims_m * rhatui_m
+//    val eq2denom = abs(sims_m) * rhatui_m.mapValues(rhatui => if (rhatui!=0) 1.0 else 0.0)
+//    //val rbarhat_m = eq2num /:/ eq2denom
+//    for ((k, numerator) <- eq2num.activeIterator) {
+//      val u = k._1
+//      val i = k._2
+//      // only need to add rbarhat if rbarhat is not zero
+//      if (eq2denom(u,i)!=0) {
+//        val rbarhat = numerator/eq2denom(u,i)
+//        rbarhat_Builder.add(u,i, rbarhat)
+//      }
+//    }
+//    val rbarhat_m = rbarhat_Builder.result()
+
+//    val rhat_ui_Builder = new CSCMatrix.Builder[Double](rows=train.rows, cols=train.cols)
+//    for ((k,rui) <- train.activeIterator) {
+//      val u = k._1
+//      val i = k._2
+//      val rhat_ui = (rui - ru_(u))/scale(rui, ru_(u))
+//      rhat_ui_Builder.add(u, i, rhat_ui)
+//    }
+//    val rhat_ui_s = rhat_ui_Builder.result()
+
+//    for ((k, rhat_ui) <- rhat_ui_s.activeIterator) {
+//      println((k._1, k._2, rhat_ui))
+//    }
+
+
+
     println("Loading test data from: " + conf.test())
     val testFile = Source.fromFile(conf.test())
-    val testBuilder = new CSCMatrix.Builder[Double](rows=conf.users(), cols=conf.movies()) 
+    val testBuilder = new CSCMatrix.Builder[Double](rows=conf.users(), cols=conf.movies())
     for (line <- testFile.getLines) {
         val cols = line.split(conf.separator()).map(_.trim)
         testBuilder.add(cols(0).toInt-1, cols(1).toInt-1, cols(2).toDouble)
@@ -49,6 +165,41 @@ object Predictor {
     testFile.close
 
     println("Compute predictions on test data...")
+    val t2 = System.nanoTime()
+    // *** compute rbarhat, the user-specific weighted-sum deviation, eq.2 in project2
+    // *** then compute pui
+    val pui_m = CSCMatrix.zeros[Double](test.rows, test.cols)  // maybe change to builder
+    for ((k, _) <- test.activeIterator) {
+      val u = k._1
+      val i = k._2
+      val sims_for_u = sims_m(u, 0 until sims_m.cols).t.toDenseVector.t
+      val rhats_for_i = rhatui_m(0 until rhatui_m.rows, i).toDenseVector
+      val numerator = sims_for_u * rhats_for_i   // maybe to dense vec
+      val denom = sims_for_u * rhats_for_i.mapValues(rhat => if (rhat!=0.0) 1.0 else 0.0)
+
+      if (denom != 0.0) {
+        val rbarhat_ui = numerator / denom
+        pui_m(u,i) = ru_arr(u) + rbarhat_ui * scale((ru_arr(u) + rbarhat_ui), ru_arr(u))
+      } else {
+        pui_m(u,i) = ru_arr(u)
+      }
+    }
+    val duration_predict = (System.nanoTime() - t2)/1e9
+    println(f"duration_predict = $duration_predict")
+
+    // *** calculate MAE ***
+    val mae = sum(abs(pui_m - test))/test.activeSize
+    println(f"mae = ${mae}")
+
+    // *** compute pui ***
+//    val rui_builder = new CSCMatrix.Builder[Double](rows=num_u, cols=num_i)
+//    for ((k, _) <- test.activeIterator) {
+//      val u = k._1
+//      val i = k._2
+//      val pui = ru_arr(u) + rbarhat_ui * scale((ru_arr(u)+rbarhat_ui), ru_arr(u))
+//
+//    }
+
 
     // Save answers as JSON
     def printToFile(content: String,
